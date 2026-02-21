8682c103de220a79d78bb4957b6eefee21d124a8
Daniel Zabat <149073202+daniel0285@users.noreply.github.com>
2026-02-21 14:33:48 +0800

    feat: implemented random fetching of pokemon from PokeAPI

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemons, setPokemons] = useState([]);
+  const [selectedPokemons, setSelectedPokemons] = useState([]);
+  const [score, setScore] = useState(0);
+
+  useEffect(() => {
+    fetchPokemons(setPokemons);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>Score: {score}</h1>
+
+      {pokemons.map((pokemon) => (
+        <button key={pokemon.id} id={pokemon.id}>
+          <img
+            key={pokemon.id}
+            id={pokemon.id}
+            src={pokemon.img}
+            alt={pokemon.name}
+          />
+          <p>{pokemon.name}</p>
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+function getRandomID() {
+  const pokemonId = [];
+  while (pokemonId.length < 10) {
+    const id = Math.floor(Math.random() * 100 + 1);
+    if (!pokemonId.includes(id)) pokemonId.push(id);
+  }
+  return pokemonId;
+}
+
+async function fetchPokemons(setState) {
+  const pokemonID = getRandomID();
+
+  try {
+    const pokemonList = await Promise.all(
+      pokemonID.map(async (id) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}/`);
+        const data = await res.json();
+
+        return {
+          name: data.name,
+          id,
+          img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+        };
+      }),
+    );
+
+    setState(pokemonList);
+  } catch (error) {
+    alert(error);
+  }
+}
+
+export default App;

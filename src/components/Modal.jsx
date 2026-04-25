1bf6cb4da12784b8f37a0b54f50008375c530255
Daniel Zabat <149073202+daniel0285@users.noreply.github.com>
2026-04-25 21:40:11 +0800

    feat: add animation styles for modal component

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,7 +12,11 @@ export function Modal({ openModal, closeModal, children }) {
   }, [openModal]);
 
   return (
-    <dialog ref={ref} onCancel={closeModal}>
+    <dialog
+      className='absolute inset-0 flex flex-col gap-5 p-10 m-auto text-center rounded-lg backdrop:backdrop-blur-sm 0 animate-zoom-in'
+      ref={ref}
+      onCancel={closeModal}
+    >
       {children}
     </dialog>
   );
